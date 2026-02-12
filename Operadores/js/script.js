0b106850e27c307c36765a2e043967d698a92a2f
= <lgmc120@gmail.com>
2026-02-12 14:22:57 -0300

    first commit

---
+++ Operadores/js/script.js
@@ -0,0 +1,10 @@
+// 1 - Number
+console.log(typeof 2)
+console.log(typeof 2.2)
+console.log(typeof -2)
+
+// 2 - Operadores
+console.log(10 - 2)
+console.log(10 + 2)
+console.log((10 + 2)* 2)
+console.log((10 + 2)* 2 / 2)
