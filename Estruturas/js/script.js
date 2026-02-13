57c557a00da2f306f0cc084186c94e60b160e017
= <lgmc120@gmail.com>
2026-02-13 01:43:57 -0300

    Math and Console

--- Estruturas/js/script.js
+++ Estruturas/js/script.js
@@ -15,8 +15,18 @@ let a = 5, b = 4, c = 10
 console.log(a+b+c)
 
 // 2 - Prompt
-nome = prompt("Escreva seu nome: ")
+// nome = prompt("Escreva seu nome: ")
 console.log(nome)
 
 //3 - Alert
-alert("Teste")
+// alert("Teste")
+
+// 4 - Math
+console.log(Math.max(1,2,3))
+console.log(Math.floor(1.2))
+console.log(Math.ceil(5.15))
+
+// 5 - Console
+console.log("A")
+console.error("Erro!")
+console.warn("Aviso!")
