569b9e226f7ae1825aebab04f5fc51d115bc2084
= <lgmc120@gmail.com>
2026-02-13 00:19:39 -0300

    Variaveis

---
+++ Estruturas/js/script.js
@@ -0,0 +1,15 @@
+// 1 - Variaveis
+let nome = 'Luis'
+console.log(nome)
+
+nome = 'Gustavo'
+console.log(nome)
+
+const idade = 23
+console.log(idade)
+
+console.log(typeof nome)
+console.log(typeof idade)
+
+let a = 5, b = 4, c = 10
+console.log(a+b+c)
