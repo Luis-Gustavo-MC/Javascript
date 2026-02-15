b8e9295281d70fe7db902f826ddd205a6d47732b
= <lgmc120@gmail.com>
2026-02-15 15:59:39 -0300

    Funções

---
+++ Funcoes/js/script.js
@@ -0,0 +1,12 @@
+// 1 - Funções
+function helloWord(){
+    console.log("Hello Word!")
+}
+helloWord()
+
+// 2 - Retornando Valores
+function somar(a,b){
+    return a + b
+}
+let a = 2 , b = 3
+console.log(somar(a,b))
