dac54d8733932348ab3c15f23d6a96ec4af49e3c
= <lgmc120@gmail.com>
2026-02-15 17:17:27 -0300

    Escopo

--- Funcoes/js/script.js
+++ Funcoes/js/script.js
@@ -10,3 +10,30 @@ function somar(a,b){
 }
 let a = 2 , b = 3
 console.log(somar(a,b))
+
+// 3 - Escopo da Função
+let u = 10
+
+function test(){
+    let u = 20
+    console.log("Variavel dentro da função "+u)
+}
+
+test()
+console.log("Variavel fora da função " + u)
+
+// 4 - Escopo aninhado
+let m = 10
+function escopoAninhado(){
+    let m = 20
+    console.log(m)
+    if(true){
+        let m = 30
+        console.log(m)
+    }if(true){
+        let m = 40
+        console.log(m)
+    }
+}
+console.log(m)
+escopoAninhado()
