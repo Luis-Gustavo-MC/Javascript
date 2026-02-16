9b441c82f767989535601def803e22a137ba602b
= <lgmc120@gmail.com>
2026-02-16 02:34:05 -0300

    Array e Propriedades

---
+++ Array_OBJ/js/script.js
@@ -0,0 +1,14 @@
+// 1 - Array
+const list = [1,2,3,4,5]
+console.log(list)
+console.log(typeof list)
+
+console.log(list[0])
+
+list.forEach(element => {
+    console.log(element)
+})
+
+// 2 - Propriedades
+const numbers = [5,3,4]
+console.log(numbers.length)
