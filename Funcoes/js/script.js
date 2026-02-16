b54c379eba22136de76f5d9a7baadbb905af116a
= <lgmc120@gmail.com>
2026-02-16 01:55:26 -0300

    Closure and Recursão

--- Funcoes/js/script.js
+++ Funcoes/js/script.js
@@ -70,8 +70,41 @@ const multiplicacao = function(m,n){
 console.log(multiplicacao(2))
 console.log(multiplicacao(2,4))
 
-// 8 - default
+// 8 - Default
 const greeting = (nome, greet = 'ola') => {
     console.log(greet + ", " + nome)
 }
 greeting('luis')
+
+// 9 - Closure
+function someFunction(){
+    let txt = "abc"
+
+    function display(){
+        console.log(txt)
+    }
+    display()
+}
+
+someFunction()
+
+const multiplicationClosure = (n) => {
+    return(m) =>{
+        return n*m
+    }
+}
+let c1 = multiplicationClosure(10) , c2 = multiplicationClosure(2)
+console.log(c1 + "\n" +  c2)
+console.log(c1(2) + "\n" +  c2(5))
+
+// 10 - Recursão
+const untilTen = (n,m) =>{
+    if(n<10){
+        console.log("A função parou de executar")
+    }else{
+        const x = n-m
+        console.log(x)
+        untilTen(x,m)
+    }
+}
+untilTen(100 , 7)
