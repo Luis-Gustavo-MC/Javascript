0876ad1651a0b7d98bf95738970155e533ec6a60
= <lgmc120@gmail.com>
2026-02-20 15:06:27 -0300

    Prototype

--- POO/js/script.js
+++ POO/js/script.js
@@ -17,3 +17,20 @@ animal.latir()
 animal.setNome('Loki')
 console.log(animal.getNome())
 
+// 2 - Prototype
+const text = "asd"
+console.log(Object.getPrototypeOf(text))
+
+const bool = true
+console.log(Object.getPrototypeOf(bool))
+
+const myObj = {
+    a:'b'
+}
+console.log(Object.getPrototypeOf(myObj))
+
+const myObj2 = Object.create(myObj)
+
+console.log(myObj2)
+
+console.log(Object.getPrototypeOf(myObj2) === myObj)
