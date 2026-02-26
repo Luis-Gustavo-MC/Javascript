4c026574353bc4327cc4cd5cd694f0e9b417d194
= <lgmc120@gmail.com>
2026-02-26 14:27:03 -0300

    Mudando HTML

--- DOM/js/script.js
+++ DOM/js/script.js
@@ -4,7 +4,7 @@ console.log(document.body.childNodes)
 console.log(document.body.childNodes[1])
 console.log(document.body.childNodes[1].textContent)
 
-// 2 GetElements
+// 2 - GetElements
 // 2.1 - Selecionando elementos por tag
 const listItens = document.getElementsByTagName("li")
 console.log(listItens)
@@ -23,3 +23,23 @@ console.log(productsQuery)
 
 const mainContainer = document.querySelector("#main-container")
 console.log(mainContainer)
+
+// 3 - Mudando HTML
+// 3.1 - insertBefore
+const p = document.createElement("p")
+const header = title.parentElement
+
+header.insertBefore(p, title)
+
+// 3.2 - appendChild
+const navLinks = document.querySelector("nav ul")
+const li = document.createElement("li")
+
+navLinks.appendChild(li)
+
+// 3.3 - replaceChild
+const h2 = document.createElement("h2")
+
+h2.textContent = "Meu novo titulo"
+
+header.replaceChild(h2 , title)
