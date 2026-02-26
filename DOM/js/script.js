716221d68c4b6bef2b7f9c5225c3c189e87d0a14
= <lgmc120@gmail.com>
2026-02-26 14:34:30 -0300

    createTextNode

--- DOM/js/script.js
+++ DOM/js/script.js
@@ -43,3 +43,13 @@ const h2 = document.createElement("h2")
 h2.textContent = "Meu novo titulo"
 
 header.replaceChild(h2 , title)
+
+// 4 - createTextNode
+const myText = document.createTextNode("Agora vamos colocar mais um titulo")
+
+console.log(myText)
+
+const h3 = document.createElement("h3")
+h3.appendChild(myText)
+
+mainContainer.appendChild(h3)
