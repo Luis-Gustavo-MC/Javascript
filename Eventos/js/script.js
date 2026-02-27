bad11074a60b695b712970779bee064c1f93a5b3
= <lgmc120@gmail.com>
2026-02-27 14:43:30 -0300

    Argumentos do eventos e Propagação

--- Eventos/js/script.js
+++ Eventos/js/script.js
@@ -20,3 +20,26 @@ btn3.addEventListener("click", () =>{
     console.log("Evento removido")
     btn2.removeEventListener("click" , imprimirMensagem)
 })
+
+// 1.3 Argumentos do eventos
+const myTitle = document.querySelector("#my-title")
+
+myTitle.addEventListener("click", (e) =>{
+    console.log(e)
+    console.log(e.offsetX)
+    console.log(e.pointerType)
+    console.log(e.target)
+})
+
+// 1.4 Propagação
+const containerBtn = document.querySelector("#btn-container")
+const btnInsiderContainer = document.querySelector("#div-btn")
+
+containerBtn.addEventListener("click", ()=>{
+    console.log("Evento1")
+})
+
+btnInsiderContainer.addEventListener("click", (e)=>{
+    e.stopPropagation()
+    console.log("Evento2")
+})
