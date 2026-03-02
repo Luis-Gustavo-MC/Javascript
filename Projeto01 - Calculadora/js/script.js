f32785ffbc7da96772a4d76528623b4f6e980c3b
= <lgmc120@gmail.com>
2026-03-02 15:23:46 -0300

    Project01 - JS part02

--- Projeto01 - Calculadora/js/script.js
+++ Projeto01 - Calculadora/js/script.js
@@ -1,1 +1,21 @@
-console.log("Teste")
+// Elementos
+const multiplicationForm = document.querySelector("#multiplication-form")
+const numberInput = document.querySelector("#number")
+const multiplicationInput = document.querySelector("#multiplicator")
+
+// Funções
+const createTable = (number,multiplicatorNumber) =>{
+    console.log(number,multiplicatorNumber)
+}
+
+// Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const multiplicationNumber = +numberInput.value
+    const multiplicatorNumber = +multiplicationInput.value
+
+    if(!multiplicationNumber || !multiplicatorNumber) return
+
+    createTable(multiplicationNumber,multiplicatorNumber)
+})
