22efb9f5f60de868ff2250a5cf0eec2c43acd6d7
= <lgmc120@gmail.com>
2026-03-03 13:13:29 -0300

    Project01 - JS part03 (end)

--- Projeto01 - Calculadora/js/script.js
+++ Projeto01 - Calculadora/js/script.js
@@ -2,10 +2,35 @@
 const multiplicationForm = document.querySelector("#multiplication-form")
 const numberInput = document.querySelector("#number")
 const multiplicationInput = document.querySelector("#multiplicator")
+const multiplicationTable = document.querySelector("#multiplication-operations")
+const multiplicationTitle = document.querySelector("#multiplication-title span")
+
 
 // Funções
 const createTable = (number,multiplicatorNumber) =>{
-    console.log(number,multiplicatorNumber)
+    multiplicationTable.innerHTML = ""
+    
+    for(i =1; i<= multiplicatorNumber; i++){
+        let aux = number*i
+        //console.log(aux)
+
+        const template = `
+        <div class="row">
+        <div class= "operation"> ${number} x ${i} = </div>
+        <div class = "result"> ${aux} </div>
+        </div>`
+
+        const parser = new DOMParser()
+
+        const htmlTemplate = parser.parseFromString(template, "text/html")
+
+        const row = htmlTemplate.querySelector(".row")
+
+         multiplicationTable.appendChild(row)
+        
+    }
+
+    multiplicationTitle.innerText = number
 }
 
 // Eventos
