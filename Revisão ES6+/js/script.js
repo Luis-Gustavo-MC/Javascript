3f50fbb21f2274f7337b2c3d86af8a3be0db1eef
= <lgmc120@gmail.com>
2026-03-10 14:21:43 -0300

    Arrow functions

--- Revisão ES6+/js/script.js
+++ Revisão ES6+/js/script.js
@@ -27,3 +27,45 @@ function logName(){
 logName()
 const name = "Luis"
 console.log(name)
+
+// 2 - Arrow functions
+// 2.1 - function normal
+const soma = function(a,b){
+    return a+b
+}
+
+// 2.2 Arrow function
+const arrowSoma = (a,b) => a+b
+
+// 2.3 Resultados
+console.log(soma(5,5))
+console.log(arrowSoma(5,5))
+
+// 2.4 Outro exemplo
+const greeting = (nome)=>{
+    if(nome){
+        return `Hello ${nome}`
+    }else{
+        return "Hello"
+    }
+}
+console.log(greeting() + " " + greeting("Luis"))
+
+const user = {
+    name: "Luis",
+    sayUserName(){
+        setTimeout(function (){
+            console.log(this)
+            console.log(`Username: ${this.name}`)
+        },1000)
+    },
+    sayUserNameArrow(){
+        setTimeout(() =>{
+            console.log(this)
+            console.log(`Username: ${this.name}`)
+        },2000)
+    }
+}
+
+user.sayUserName()
+user.sayUserNameArrow()
