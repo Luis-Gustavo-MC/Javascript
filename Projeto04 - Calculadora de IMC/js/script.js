2b0567251f2c036514c141b4bcea467c07d4beb0
= <lgmc120@gmail.com>
2026-03-10 13:50:29 -0300

    Project04 - js part05 end

--- Projeto04 - Calculadora de IMC/js/script.js
+++ Projeto04 - Calculadora de IMC/js/script.js
@@ -77,6 +77,8 @@ function createTable(data) {
 function cleanInputs(){
   heightInput.value = ""
   weightInput.value = ""
+  imcNumber.classList = ""
+  imcInfo.classList = ""
 }
 
 function validDigits(text){
@@ -123,6 +125,30 @@ calcBtn.addEventListener('click' , (e) =>{
   imcNumber.innerText = imc
   imcInfo.innerText = info
 
+  switch(info){
+    case "Magreza":
+      imcNumber.classList.add("low")
+      imcInfo.classList.add("low")
+    break
+    case "Normal":
+      imcNumber.classList.add("good")
+      imcInfo.classList.add("good")
+    break
+    case "Sobrepeso":
+      imcNumber.classList.add("low")
+      imcInfo.classList.add("low")
+    break
+    case "Obesidade":
+      imcNumber.classList.add("medium")
+      imcInfo.classList.add("medium")
+    break
+    case "Obesidade grave":
+      imcNumber.classList.add("high")
+      imcInfo.classList.add("high")
+    break
+  }
+
+
   showOrHideResults()
 })
 
@@ -131,4 +157,8 @@ clearBtn.addEventListener("click" , (e) =>{
   cleanInputs()
 })
 
+backBtn.addEventListener('click' , ()=>{
+  cleanInputs()
+  showOrHideResults()
+})
 
