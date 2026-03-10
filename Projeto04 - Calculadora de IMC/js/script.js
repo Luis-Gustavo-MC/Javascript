8feda6da3ef42d3360311641a684953cd4ba3257
= <lgmc120@gmail.com>
2026-03-10 13:04:03 -0300

    Project04 - js part04.2 fix bug

--- Projeto04 - Calculadora de IMC/js/script.js
+++ Projeto04 - Calculadora de IMC/js/script.js
@@ -41,6 +41,7 @@ const data = [
 const imcTable = document.querySelector("#imc-table")
 const heightInput = document.querySelector("#height")
 const weightInput = document.querySelector("#weight")
+
 const calcBtn = document.querySelector("#calc-btn")
 const clearBtn = document.querySelector("#clear-btn")
 const imcNumber = document.querySelector("#imc-number span")
@@ -49,27 +50,28 @@ const imcInfo = document.querySelector("#imc-info span")
 const backBtn = document.querySelector("#back-btn")
 const calcContainer = document.querySelector("#calc-container");
 const resultContainer = document.querySelector("#result-container");
+ 
 // Functions
-function createTable(data){
-    data.forEach((item)=>{
-        const div = document.createElement("div")
-        div.classList.add("table-data")
+function createTable(data) {
+  data.forEach((item) => {
+    const div = document.createElement("div")
+    div.classList.add("table-data")
 
-        const classification = document.createElement("p")
-        classification.innerHTML = item.classification
+    const classification = document.createElement("p")
+    classification.innerText = item.classification;
 
-        const info = document.createElement("p")
-        info.innerHTML = item.info
+    const info = document.createElement("p")
+    info.innerText = item.info;
 
-        const obesity = document.createElement("p")
-        obesity.innerHTML = item.obesity
+    const obesity = document.createElement("p")
+    obesity.innerText = item.obesity;
 
-        div.appendChild(classification)
-        div.appendChild(info)
-        div.appendChild(obesity)
+    div.appendChild(classification)
+    div.appendChild(info)
+    div.appendChild(obesity)
 
-        imcTable.appendChild(div)
-    })
+    imcTable.appendChild(div)
+  });
 }
 
 function cleanInputs(){
@@ -91,6 +93,10 @@ function showOrHideResults(){
   resultContainer.classList.toggle("hide")
 }
 
+// Inicialization
+createTable(data);
+
+//Eventos
 [heightInput, weightInput].forEach((el) => {
   el.addEventListener("input", (e) => {
     const updatedValue = validDigits(e.target.value)
@@ -98,10 +104,6 @@ function showOrHideResults(){
   })
 })
 
-// Inicialization
-createTable(data)
-
-//Eventos
 calcBtn.addEventListener('click' , (e) =>{
   e.preventDefault()
   const weight = +weightInput.value.replace("," , ".")
