5d5aa19c2a7c07c087c350fb51625713e585978f
= <lgmc120@gmail.com>
2026-03-12 15:02:41 -0300

    POO

--- Revisão ES6+/js/script.js
+++ Revisão ES6+/js/script.js
@@ -145,3 +145,38 @@ const otherInfos = {km: 100000, price: 22000}
 
 const car = {...carName, ...carBrand, ...otherInfos}
 console.log(car)
+
+// 7 - POO
+// 7.1 - Classes
+class Product{
+    constructor(name,price){
+        this.name = name
+        this.price = price
+    }
+    productWithDiscount(discount){
+        return this.price * ((100 - discount) / 100)
+    }
+    toString(){
+        return `Nome: ${this.name} \nPreço: ${price} `
+    }
+}
+const shirt = new Product("Camisa" , 20)
+console.log(shirt.toString())
+console.log(shirt.productWithDiscount(15))
+
+// 7.2 Herança
+class ProductWithAttributes extends Product{
+    constructor(name,price,colors){
+        super(name,price)
+        this.colors = colors
+    }
+    showColors(){
+        console.log(`As cores são: `)
+        this.colors.forEach((color) => {
+            console.log(color)
+        });
+    }
+}
+const hat = new ProductWithAttributes("Chapeu", 29.99, ["Preto", "Amarelo", "Verde"])
+console.log(hat.toString())
+hat.showColors()
