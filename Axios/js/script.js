057e8275d36a6ba0291feadda355126f4f154fa3
= <lgmc120@gmail.com>
2026-03-13 14:33:35 -0300

    imprimindo dados

--- Axios/js/script.js
+++ Axios/js/script.js
@@ -8,10 +8,31 @@ const getData = async() => {
         console.log(response)
 
         return response.data
-        
+
     }catch(error){
         console.log(error)
     }
 }
-
 getData()
+
+// 3 - imprimindo dados 
+const container = document.querySelector("#user-container")
+const printData = async() => {
+    const data = await getData()
+
+    data.forEach((user) => {
+        const div = document.createElement("div")
+        const nameElement = document.createElement("h2")
+
+        nameElement.textContent = user.name
+        div.appendChild(nameElement)
+        
+        const emailElement = document.createElement("P")
+        emailElement.textContent = user.email
+        div.appendChild(emailElement)
+    
+        container.appendChild(div)
+    });
+} 
+
+printData()
