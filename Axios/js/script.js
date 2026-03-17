6b0a31b815d89b6ddaf769136da36855eec2f1f4
= <lgmc120@gmail.com>
2026-03-17 14:19:06 -0300

    Axios: Post

--- Axios/js/script.js
+++ Axios/js/script.js
@@ -44,3 +44,15 @@ const printData = async() => {
 } 
 printData()
 
+//  5 - Post
+const form = document.querySelector("#post-form")
+const titleInput = document.querySelector("#title")
+const bodyInput = document.querySelector("#body")
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault()
+
+    axios.post("https://jsonplaceholder.typicode.com/posts",{
+        body: {title: titleInput.value, body: bodyInput.value, userId:1},
+    });
+});
